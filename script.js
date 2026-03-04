ef3bf058b2c460c762ae8a23b8f3b3fa97531d7c
indritrexha <indritrexha144@gmail.com>
2026-03-04 15:16:32 +0100

    Initial to-do app

---
+++ script.js
@@ -0,0 +1,31 @@
+alert("JS is working");
+
+const taskInput =
+document.getElementById("taskInput");
+const addBtn =
+document.getElementById("addBtn");
+const taskList =
+document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+  if (taskInput.value === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskInput.value;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn =
+document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.onclick = () => li.remove();
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
